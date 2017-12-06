5f3f8e09c3d4adf224f58e0c84f80de7dbac7933
Shadowsental <33833247+Shadowsental@users.noreply.github.com>
2017-12-06 11:41:14 +0100

    Robbie Rotten

--- Project Vampire Hunter/Assets/VampireHunterScript/NextStageScript.cs
+++ Project Vampire Hunter/Assets/VampireHunterScript/NextStageScript.cs
@@ -1,16 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class NextStageScript : MonoBehaviour {
+public class NextStageScript : MonoBehaviour{
 
-	// Use this for initialization
-	void Start () {
-		
+	public GameObject guiObject;
+	public string levelToLoad;
+
+	void Start()
+	{
+		guiObject.SetActive (false);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			guiObject.SetActive(true);
+			if(guiObject.activeInHierarchy == true && Input.GetButtonDown("Use"))
+			{
+				SceneManager.LoadScene(levelToLoad);
+			}
+		}
 	}
+
+	void OnTriggerExit2D()
+	{
+		guiObject.SetActive (false);
+	}
+
 }
